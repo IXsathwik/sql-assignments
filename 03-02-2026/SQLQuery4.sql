0e06b64c76438a99a787a622e7bab43324cd3c76
IXsathwik <sathwik.thotapelli@innovatechs.com>
2026-02-03 15:33:40 +0530

    second commit

--- 03-02-2026/SQLQuery4.sql
+++ 03-02-2026/SQLQuery4.sql
@@ -46,25 +46,3 @@ select * from students where age is not NULL;
 
 
 
-
-
-
-
-TASK 4  
- — WHERE  
-Select students age > 21 
-Select active students only 
-Select courses with fee BETWEEN 1000 AND 5000 
-Select students with age IN (20, 22, 25) 
- — AND / OR 
-Students age > 20 AND active 
-Students age < 20 OR inactive 
-Courses with fee > 2000 AND name LIKE '%Data%' 
- — LIKE Pattern 
-Emails ending with ‘gmail.com’ (%gmail.com) 
-Course names starting with ‘Data’ (Data%) 
-Cities containing ‘new’ anywhere (%new%) 
- — NULL Checks 
-Insert a student with NULL age 
-Select students where age IS NULL 
-Select students where age IS NOT NULL 
